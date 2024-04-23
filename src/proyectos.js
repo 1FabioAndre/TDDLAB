f15fb4c3119b8a599e34a14bdc2cdf25be9f3d88
Patrick <patrickgonzalesquintana@gmail.com>
2024-04-23 00:24:15 -0400

    Se mejoro la funcion addProject para que se puedan añadir mas titulos de proyectos

--- src/proyectos.js
+++ src/proyectos.js
@@ -1,5 +1,10 @@
+let proyectos = []; // Array para almacenar los títulos de los proyectos
+
 function addProject(titulo = "") {
-    return [titulo];
+    if (titulo.trim() !== "") { // Verificar si el título no está vacío después de eliminar espacios en blanco
+        proyectos.push(titulo);
+    }
+    return proyectos;
 }
 
 export default addProject;
--- src/proyectos.spec.js
+++ src/proyectos.spec.js
@@ -3,10 +3,14 @@ import addProject from "./proyectos";
 
 describe("Añadir proyectos", () => {
     it("Deberia mostrar una array vacio", () => {
-        expect(addProject()).toEqual([""]);
+        expect(addProject()).toEqual([]);
     });
 
     it("Deberia mostrar una array con el titulo de un proyecto", () => {
         expect(addProject("Titulo")).toEqual(["Titulo"]);
+    });
+
+    it("Deberia mostrar una array con 2 titulos de proyectos", () => {
+        expect(addProject("Titulo2")).toEqual(["Titulo", "Titulo2"]);
     });
 });
